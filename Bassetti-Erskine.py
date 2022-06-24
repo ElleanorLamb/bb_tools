e2a99f843c60b440d47640888565e5019b1ac95d
sterbini <guido.sterbini@cern.ch>
2022-06-24 19:48:59 +0200

    A small typo

--- Bassetti-Erskine.py
+++ Bassetti-Erskine.py
@@ -1,28 +1,29 @@
+# %%
 import numpy as np 
 import scipy
 from scipy import constants
 import scipy.special as ss 
 from matplotlib import pyplot as plt 
 
+# %%
 
 q = scipy.constants.e
 c = scipy.constants.c
 m_p = scipy.constants.proton_mass
 pi = np.pi
 eps0 = scipy.constants.epsilon_0
-N=1.8e11
-gamma=6800/0.938
+N  = 1.8e11
+gamma = 6800/0.938
 beta = np.sqrt(1-1/gamma**2)
 r_p = q*q/(4*np.pi*eps0*c*c*m_p) # proton radius
 
 # BASSETTI-ERSKINE
 
+sig_y = 1e-5
+sig_x = sig_y*1.0001
 
-sig_x = np.sqrt(1.001e-10) 
-sig_y = np.sqrt(1e-10)
 
-
-x=np.linspace(sig_x*30,-sig_x*30,1000)
+x=np.linspace(-sig_x*30,sig_x*30,1000)
 y = 0
 z = [] # complex of x and y
 for i in range(len(x)):
@@ -31,10 +32,10 @@ for i in range(len(x)):
 kick_BE = []
 Force_BE = []
 for i in range(len(x)):
-    l = (N*q)/(2*eps0*np.sqrt(2*pi*sig_x**2-sig_y**2))
-    m = ss.wofz(z[i]/(np.sqrt(2*sig_x**2-sig_y**2)))
+    l = (N*q)/(2*eps0*np.sqrt(2*pi*(sig_x**2-sig_y**2)))
+    m = ss.wofz(z[i]/(np.sqrt(2*(sig_x**2-sig_y**2))))
     n = np.exp(-x[i]**2/(2*sig_x**2)-y**2/(2*sig_y**2))
-    o = ss.wofz((x[i]*sig_y/sig_x+1j*y*sig_x/sig_y)/np.sqrt(2*sig_x**2-sig_y**2))
+    o = ss.wofz((x[i]*sig_y/sig_x+1j*y*sig_x/sig_y)/np.sqrt(2*(sig_x**2-sig_y**2)))
     Force = l*(m-n*o)*q
     Force_BE.append(Force)
                     
@@ -44,19 +45,21 @@ for i in range(len(x)):
     
 # ROUND BEAMS 
 
-sigma = np.sqrt(1.000e-10)
+sigma = sig_y
 
 kick_RB = []
 radius=[]
 for i in range(len(x)):
     radius= (np.sqrt(x[i]**2+y**2))  # radius of x,y 
-    kick_RB.append((-2*N*r_p/gamma)*(x[i]/radius**2)*(1-np.exp(-radius**2/(2*sigma**2)))) # why negative? 
+    kick_RB.append((2*N*r_p/gamma)*(x[i]/radius**2)*(1-np.exp(-radius**2/(2*sigma**2)))) # why negative? 
     
 # plot 
     
-plt.plot(x/sig_x,np.imag(kick_BE), label='Bassetti Erskine $\sigma_x$=2*$\sigma_y$')
-plt.plot(x/sigma,kick_RB, label='RB $\sigma_x$=$\sigma_y$')
+plt.plot(x/sig_x,np.imag(kick_BE), label='Bassetti Erskine $\sigma_x$=1.0001*$\sigma_y$')
+plt.plot(x/sigma,kick_RB, '.',label='RB $\sigma_x$=$\sigma_y$')
 plt.xlabel('$\sigma_x$')
 plt.ylabel('$\Delta x\'$')
 plt.title('Kick in x, y=0')
 plt.legend()
+
+# %%
