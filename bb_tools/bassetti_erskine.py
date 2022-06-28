9f61e72d3a386d876c224126b81632cd3eb07a5f
ElleanorLamb <75122689+ElleanorLamb@users.noreply.github.com>
2022-06-28 12:19:59 +0200

    Update bassetti_erskine.py
    Descriptions of functions added

--- bb_tools/bassetti_erskine.py
+++ bb_tools/bassetti_erskine.py
@@ -14,7 +14,8 @@ r_p = q*q/(4*pi*eps0*c*c*m_p) # proton radius
 
 def bassetti_erskine_kick(x, y, N, sig_x, sig_y, gamma, species='proton'):
     '''
-    TODO
+    The kick delta x due to a thin element beam beam lens for Head On interactions 
+    for proton beams. 
     '''
     
     if not (species == 'proton'):
@@ -26,7 +27,7 @@ def bassetti_erskine_kick(x, y, N, sig_x, sig_y, gamma, species='proton'):
 
 def bassetti_erskine_force(x, y, N, sig_x,sig_y):
     '''
-    TODO
+    The force due to a thin beam beam interaction for HO collisions for proton beams. 
     https://accelconf.web.cern.ch/hb2016/papers/mopr025.pdf
     '''
     z = complex(x,y)
@@ -39,7 +40,8 @@ def bassetti_erskine_force(x, y, N, sig_x,sig_y):
     
 def round_gaussian_force(x, y, N, sig):
     '''
-    TODO
+    In the round beam approximation, sig_x=sig_y, the force due to a HO beam-beam interaction 
+    for protons. 
     '''
     radius = np.sqrt(x**2+y**2)
     z = complex(y, x)
@@ -48,7 +50,8 @@ def round_gaussian_force(x, y, N, sig):
 
 def round_gaussian_kick(x, y, N, sig, gamma, species='proton'):
     '''
-    TODO
+    In the round beam approximation, the kick due to a beam beam HO interaction for protons. 
+    https://cdsweb.cern.ch/record/1982430/files/431-459%20Herr.pdf
     '''
     if not (species == 'proton'):
         raise NotImplementedError
